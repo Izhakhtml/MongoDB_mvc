562efa5521796f35dd8187540cbfca5dcc829d8c
Izhakhtml <Izhaklijalem28@gmail.com>
2022-02-28 17:07:01 +0200

    app changes

--- server/Server.js
+++ server/Server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 require('./DB/index');
 const express = require('express');
 const cors = require('cors');
+const EmployeesRouter = require('./Routes/employeesRoter');
 const App = express();
 const port = process.env.PORT || 8000
 App.use(cors());
 App.use(express.json());
-
-const EmployeesRouter = require('./Routes/employeesRoter');
 App.use('/api',EmployeesRouter);
 App.listen(port);
+App.get("/",(req,res)=>{
+    res.send('<h1>wellcome there;</h1>')
+})
